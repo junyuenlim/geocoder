c9b49df7125912a805ba41c3c2c357c5be30d5d1
Jun Lim <junyuen@gmail.com>
2014-03-22 15:06:23 +0800

    geocoder with locations scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geocoder::Application.config.secret_key_base = '14176756c5d4f19612f3c66cd4f3578193cf62bcb0782b8d8197a990c188eeeef3675a75d574f510815732517a721cd9b0a6424d0f5e1ff99333b89b7632c6b0'
